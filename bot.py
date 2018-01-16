c854d236b684790ede9619c5a4c060ed08103e13
Vaddyl <29392110+Vaddyl@users.noreply.github.com>
2018-01-16 11:32:10 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from telegram.ext import Updater, CommandHandler
+import os
+
+#           Config vars
+token = os.environ['TELEGRAM_TOKEN']
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+updater = Updater('token')
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
