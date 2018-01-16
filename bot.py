f0f50e3af200108d04be81b2b0009dbf7273538d
Vaddyl <29392110+Vaddyl@users.noreply.github.com>
2018-01-16 11:49:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ def hello(bot, update):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
 
-updater = Updater('token')
+updater = Updater(token)
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
 updater.start_polling()
