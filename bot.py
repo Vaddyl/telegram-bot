9d3d491b9694c14d2e208bdec7fb48131fadab99
Vaddyl <29392110+Vaddyl@users.noreply.github.com>
2018-01-16 14:26:43 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,12 +11,15 @@ def hello(bot, update):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
     
-
+def uknown(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Please use the available command, use /start command to see any available command")
+    
 updater = Updater(token)
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('hello', hello))
+dispatcher.add_handler(CommandHandler(Filters.command, uknown))
 
 updater.start_polling()
 updater.idle()
